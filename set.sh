a0dbf2d85164c73125d7b5a5468623e8e2e8e9bc
martin <martin@localhost.localdomain>
2020-10-24 09:27:00 +0200

    initial commit

---
+++ set.sh
@@ -0,0 +1,5 @@
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export PATH=$PATH:/opt/Xilinx/SDK/2019.1/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/:/opt/Xilinx/Vivado/2020.1/bin/
+export VIVADO_SETTINGS=/opt/Xilinx/Vivado/2020.1/settings64.sh
+
+export VIVADO_VERSION=v2020.1
