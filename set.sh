82460e5891f4113263aa0249cb7ecb052a531930
martin <martin@localhost.localdomain>
2020-10-24 10:27:42 +0200

    with the 2018.3 version
    [martin@localhost pluto]$ make
    Building util_cdc library [/home/martin/src/hdl/library/util_cdc/util_cdc_ip.log] ... OK
    Building util_axis_fifo library [/home/martin/src/hdl/library/util_axis_fifo/util_axis_fifo_ip.log] ... OK
    Building axi_dmac library [/home/martin/src/hdl/library/axi_dmac/axi_dmac_ip.log] ... OK
    Building util_fir_dec library [/home/martin/src/hdl/library/util_fir_dec/util_fir_dec_ip.log] ... OK
    Building util_fir_int library [/home/martin/src/hdl/library/util_fir_int/util_fir_int_ip.log] ... OK
    Building pluto project [/home/martin/src/hdl/projects/pluto/pluto_vivado.log] ... FAILED
    For
    
    connect_bd_net /axi_ad9361_dac_dma/fifo_rd_underflow /axi_ad9361/dac_dunf
    WARNING: [BD 41-1306] The connection to interface pin /axi_ad9361_dac_dma/fifo_rd_underflow is being overridden by the user. This pin will not be connected as a part of interface connection fifo_rd
    connect_bd_net GND_16/dout axi_ad9361/dac_data_i1
    connect_bd_net GND_16/dout axi_ad9361/dac_data_q1
    WARNING: [BD 5-232] No interface pins matched 'get_bd_intf_pins -filter {MODE == Slave && VLNV == xilinx.com:interface:aximm_rtl:1.0} -of_objects /axi_ad9361'
    connect_bd_net -net /sys_cpu_clk /axi_cpu_interconnect/M01_ACLK
    connect_bd_net -net /sys_cpu_resetn /axi_cpu_interconnect/M01_ARESETN
    connect_bd_intf_net /axi_cpu_interconnect/M01_AXI
    ERROR: [BD 5-106] Arguments to the connect_bd_intf_net command cannot be empty.
    ERROR: [Common 17-39] 'connect_bd_intf_net' failed due to earlier errors.
    
        while executing
    "connect_bd_intf_net $m_name_1 $m_name_2"
        invoked from within
    "if {[get_property CLASS $m_name_1] eq "bd_intf_pin"} {
        puts "connect_bd_intf_net $m_name_1 $m_name_2"
        connect_bd_intf_net $m_name_1 $m_name_2
    ..."
        (procedure "ad_connect" line 44)
        invoked from within
    "ad_connect axi_cpu_interconnect/${i_str}_AXI ${p_intf}"
        (procedure "ad_cpu_interconnect" line 127)
        invoked from within
    "ad_cpu_interconnect 0x79020000 axi_ad9361"
        (file "system_bd.tcl" line 229)
    
        while executing
    "source system_bd.tcl"
        (procedure "adi_project" line 132)
        invoked from within
    "adi_project pluto"
        (file "system_project.tcl" line 7)
    INFO: [Common 17-206] Exiting Vivado at Sat Oct 24 10:27:15 2020...

--- set.sh
+++ set.sh
@@ -1,7 +1,7 @@
 export CROSS_COMPILE=arm-linux-gnueabihf-
-export PATH=$PATH:/opt/Xilinx/SDK/2019.1/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/:/opt/Xilinx/Vivado/2020.1/bin/
+export PATH=$PATH:/opt/Xilinx/SDK/2018.3/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/:/opt/Xilinx/Vivado/2018.3/bin/
 export VIVADO_SETTINGS=/opt/Xilinx/Vivado/2020.1/settings64.sh
-
-export VIVADO_VERSION=v2020.1
+ 
+export VIVADO_VERSION=v2018.3
 
 #git checkout    remotes/origin/hdl_2019_r1
