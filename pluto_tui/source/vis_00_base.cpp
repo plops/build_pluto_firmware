74a57a9ec7d62f33d0f312b967a2e515d254461b
martin <martin@localhost.localdomain>
2020-10-26 19:28:38 +0100

    sampling
    295871495644 3070180496 vis_00_base.cpp:213 main   nbytes='1024'
    295871962996 3070180496 vis_00_base.cpp:213 main   nbytes='1024'
    295872334483 3070180496 vis_00_base.cpp:213 main   nbytes='1024'
    295872759709 3070180496 vis_00_base.cpp:213 main   nbytes='1024'
    295873179298 3070180496 vis_00_base.cpp:213 main   nbytes='1024

--- pluto_tui/source/vis_00_base.cpp
+++ pluto_tui/source/vis_00_base.cpp
@@ -26,12 +26,12 @@ using namespace std::chrono_literals;
 // implementation
 State state;
 int main(int argc, char **argv) {
-  state._code_version = "f683b7d35136d37481a6f48f244e7212a98245d1";
+  state._code_version = "42cf69f118987c5a770583a617f125f9dde91a3d";
   state._code_repository =
       "https://github.com/plops/build_pluto_firmware/tree/master/pluto_tui";
   state._code_author = "Martin Kielhorn <kielhorn.martin@gmail.com>";
   state._code_license = "GPL v3";
-  state._code_generation_time = "19:21:25 of Monday, 2020-10-26 (GMT+1)";
+  state._code_generation_time = "19:28:16 of Monday, 2020-10-26 (GMT+1)";
   state._start_time =
       std::chrono::high_resolution_clock::now().time_since_epoch().count();
 
@@ -195,27 +195,30 @@ int main(int argc, char **argv) {
       << (std::flush);
   iio_channel_enable(rx_i);
   iio_channel_enable(rx_q);
-  auto const nbuf = 1024;
+  auto const nbuf = 256;
   auto input = std::array<std::complex<float>, nbuf>();
   auto rxbuf = iio_device_create_buffer(rx, nbuf, false);
-  auto nbytes = iio_buffer_refill(rxbuf);
-  auto step = iio_buffer_step(rxbuf);
-  auto end = iio_buffer_end(rxbuf);
-  auto start = static_cast<uint8_t *>(iio_buffer_first(rxbuf, rx_i));
-  auto i = 0;
+  for (auto j = 0; (j) < (100); (j) += (1)) {
+    auto nbytes = iio_buffer_refill(rxbuf);
+    auto step = iio_buffer_step(rxbuf);
+    auto end = iio_buffer_end(rxbuf);
+    auto start = static_cast<uint8_t *>(iio_buffer_first(rxbuf, rx_i));
+    auto i = 0;
 
-  (std::cout)
-      << (std::setw(10))
-      << (std::chrono::high_resolution_clock::now().time_since_epoch().count())
-      << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__) << (":")
-      << (__LINE__) << (" ") << (__func__) << (" ") << ("") << (" ")
-      << (std::setw(8)) << (" nbytes='") << (nbytes) << ("'") << (std::endl)
-      << (std::flush);
-  for (uint8_t *p = start; (p) < (end); (p) += (step)) {
-    auto si = reinterpret_cast<int16_t *>(p)[0];
-    auto sq = reinterpret_cast<int16_t *>(p)[1];
-    input[i] = std::complex<float>(si, sq);
-    (i)++;
+    (std::cout) << (std::setw(10))
+                << (std::chrono::high_resolution_clock::now()
+                        .time_since_epoch()
+                        .count())
+                << (" ") << (std::this_thread::get_id()) << (" ") << (__FILE__)
+                << (":") << (__LINE__) << (" ") << (__func__) << (" ") << ("")
+                << (" ") << (std::setw(8)) << (" nbytes='") << (nbytes) << ("'")
+                << (std::endl) << (std::flush);
+    for (uint8_t *p = start; (p) < (end); (p) += (step)) {
+      auto si = reinterpret_cast<int16_t *>(p)[0];
+      auto sq = reinterpret_cast<int16_t *>(p)[1];
+      input[i] = std::complex<float>(si, sq);
+      (i)++;
+    }
   }
   iio_context_destroy(ctx);
   return 0;
