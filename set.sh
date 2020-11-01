ed2ceaf0447eecd3ce891cd959f2ada3c4864f72
martin <martin@localhost.localdomain>
2020-11-01 22:43:56 +0100

    try to build hdl

--- set.sh
+++ set.sh
@@ -1,8 +1,10 @@
+unset LD_LIBRARY_PATH
+export ADI_USE_OOC_SYNTHESIS=y
 export CROSS_COMPILE=arm-linux-gnueabihf-
-export PATH=$PATH:/opt/Xilinx/SDK/2018.3/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/:/opt/Xilinx/Vivado/2018.3/bin/
-export VIVADO_SETTINGS=/opt/Xilinx/Vivado/2020.1/settings64.sh
+export PATH=$PATH:/opt/Xilinx/SDK/2019.1/gnu/aarch32/lin/gcc-arm-linux-gnueabi/bin/:/opt/Xilinx/Vivado/2019.1/bin/
+export VIVADO_SETTINGS=/opt/Xilinx/Vivado/2019.1/settings64.sh
  
-export VIVADO_VERSION=v2018.3
+export VIVADO_VERSION=v2019.1
 
 #git clone https://github.com/analogdevicesinc/hdl/
 #git checkout    remotes/origin/hdl_2019_r1
