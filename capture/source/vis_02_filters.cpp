f6bd870ba7c76df7bcf3c5ef810c7705ccaf6d06
martin <martin@localhost.localdomain>
2020-11-12 20:40:31 +0100

    start filter code

---
+++ capture/source/vis_02_filters.cpp
@@ -0,0 +1,84 @@
+
+#include "utils.h"
+
+#include "globals.h"
+
+extern State state;
+#include <chrono>
+#include <iostream>
+#include <thread>
+float filter_2_low_ 1_real(float xn) {
+  static float yn1 = (0.f);
+  static float yn2 = (0.f);
+  static float xn1 = (0.f);
+  static float xn2 = (0.f);
+  float yn = (0.f);
+  yn = (((((8.664870e-4f)) * (xn))) + ((((1.7326780e-3f)) * (xn1))) +
+        ((((8.663387e-4f)) * (xn2))) + ((((1.919129f)) * (yn1))) +
+        ((((-0.9225943f)) * (yn2))));
+  xn2 = xn1;
+  xn1 = xn;
+  yn2 = yn1;
+  yn1 = yn;
+  return yn;
+}
+float filter_2_low_ 10_real(float xn) {
+  static float yn1 = (0.f);
+  static float yn2 = (0.f);
+  static float xn1 = (0.f);
+  static float xn2 = (0.f);
+  float yn = (0.f);
+  yn = (((((6.37280e-2f)) * (xn))) + ((((0.1274560f)) * (xn1))) +
+        ((((6.37280e-2f)) * (xn2))) + ((((1.1943650f)) * (yn1))) +
+        ((((-0.4492774f)) * (yn2))));
+  xn2 = xn1;
+  xn1 = xn;
+  yn2 = yn1;
+  yn1 = yn;
+  return yn;
+}
+float filter_2_low_ 40_real(float xn) {
+  static float yn1 = (0.f);
+  static float yn2 = (0.f);
+  static float xn1 = (0.f);
+  static float xn2 = (0.f);
+  float yn = (0.f);
+  yn = (((((0.6362308f)) * (xn))) + ((((1.2724620f)) * (xn1))) +
+        ((((0.6362308f)) * (xn2))) + ((((-1.125379f)) * (yn1))) +
+        ((((-0.4195441f)) * (yn2))));
+  xn2 = xn1;
+  xn1 = xn;
+  yn2 = yn1;
+  yn1 = yn;
+  return yn;
+}
+float filter_2_high_ 1_real(float xn) {
+  static float yn1 = (0.f);
+  static float yn2 = (0.f);
+  static float xn1 = (0.f);
+  static float xn2 = (0.f);
+  float yn = (0.f);
+  yn = (((((0.9567529f)) * (xn))) + ((((-1.9135060f)) * (xn1))) +
+        ((((0.9567529f)) * (xn2))) + ((((1.9114370f)) * (yn1))) +
+        ((((-0.9155749f)) * (yn2))));
+  xn2 = xn1;
+  xn1 = xn;
+  yn2 = yn1;
+  yn1 = yn;
+  return yn;
+}
+float filter_2_high_ 40_real(float xn) {
+  static float yn1 = (0.f);
+  static float yn2 = (0.f);
+  static float xn1 = (0.f);
+  static float xn2 = (0.f);
+  float yn = (0.f);
+  yn = (((((6.37280e-2f)) * (xn))) + ((((-0.1274560f)) * (xn1))) +
+        ((((6.37280e-2f)) * (xn2))) + ((((-1.1943650f)) * (yn1))) +
+        ((((-0.4492774f)) * (yn2))));
+  xn2 = xn1;
+  xn1 = xn;
+  yn2 = yn1;
+  yn1 = yn;
+  return yn;
+}
---
+++ capture/source/vis_02_filters.hpp
@@ -0,0 +1,16 @@
+#ifndef VIS_02_FILTERS_H
+#define VIS_02_FILTERS_H
+#include "utils.h"
+;
+#include "globals.h"
+;
+#include <chrono>
+#include <iostream>
+#include <thread>
+;
+float filter_2_low_ 1_real(float xn);
+float filter_2_low_ 10_real(float xn);
+float filter_2_low_ 40_real(float xn);
+float filter_2_high_ 1_real(float xn);
+float filter_2_high_ 40_real(float xn);
+#endif
